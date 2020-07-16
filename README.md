003d57f6f4302a66768de726cdb7abcbdc16f712
Truong Nguyen <minhtruong315@gmail.com>
2020-07-16 14:06:25 +0700

    add opencart website

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# fluxstore-opencart
-Pre-config Opencart Website
+# Demo Opencart website for Fluxstore, MStore app
+[![N|Solid](http://news.inspireui.com/wp-content/uploads/2017/06/powerbuy-1.png)](http://inspireUI.com)
+
+### Installation
+-   Create a database in mysql and import database file (database/opencart.sql)
+-   Change the config.php and admin/config.php to your database connection and your website.
+    ```
+    define('WEBSITE_URL', 'http://localhost:8888/fluxstore_opencart/');
+    define('WEBSITE_DIRECTORY', '/Users/admin/Documents/Research/fluxstore_opencart/');
+    define('CACHE_STORAGE_DIRECTORY', '/Users/admin/Documents/storage/');
+
+
+    define('DB_HOSTNAME', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_DATABASE', 'fluxstore_opencart');
+    ```
+-   Account login: admin / admin
+
+-   ##### MStore app:
+    -   Update file *src/common/AppConfig.json* to change new Opencart URL
+    
+-   ##### FluxStore app:
+    -   Copy file *example/opencart/config.dart* and *example/opencart/config_en.json* and override the files in *lib/common* folder and change url website as well.
+
+### Support
+Please post the issue ticket with your Envato username and purchase code to ask for installing help or troubleshooting from InspireUI support team - https://github.com/inspireui/support/issues
+        
+Thank you very much!
---
+++ admin/config.php
@@ -0,0 +1,40 @@
+<?php
+define('WEBSITE_URL', 'http://localhost:8888/fluxstore_opencart/');
+define('WEBSITE_DIRECTORY', '/Users/admin/Documents/Research/fluxstore_opencart/');
+define('CACHE_STORAGE_DIRECTORY', '/Users/admin/Documents/storage/');
+
+// HTTP
+define('HTTP_SERVER', WEBSITE_URL.'/admin/');
+define('HTTP_CATALOG', WEBSITE_URL);
+
+// HTTPS
+define('HTTPS_SERVER', WEBSITE_URL.'admin/');
+define('HTTPS_CATALOG', WEBSITE_URL);
+
+// DIR
+define('DIR_APPLICATION', WEBSITE_DIRECTORY.'admin/');
+define('DIR_SYSTEM', WEBSITE_DIRECTORY.'system/');
+define('DIR_IMAGE', WEBSITE_DIRECTORY.'image/');
+define('DIR_STORAGE', CACHE_STORAGE_DIRECTORY);
+define('DIR_CATALOG', WEBSITE_DIRECTORY.'catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'fluxstore_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
